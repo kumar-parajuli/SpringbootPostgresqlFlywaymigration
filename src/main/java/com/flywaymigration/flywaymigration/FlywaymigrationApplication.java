464babe061ebfc448f00a4a8c0c3cbbb1e1981a3
parajuli rahul <parajulirahul98@gmail.com>
2024-03-20 22:00:31 +0545

    config the postgresqldatabase

---
+++ src/main/java/com/flywaymigration/flywaymigration/FlywaymigrationApplication.java
@@ -0,0 +1,14 @@
+package com.flywaymigration.flywaymigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaymigrationApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FlywaymigrationApplication.class, args);
+	}
+
+}
